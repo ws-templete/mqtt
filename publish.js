be123473c08a9fb1eaa3bcf4d8266a33af0faf25
杨国威 <yangguowei@uino.com>
2023-06-16 10:55:37 +0800

    feat: mqtt服务

--- publish.js
+++ publish.js
@@ -1,23 +1,24 @@
-const mqtt = require("mqtt");
-const m1 = require("./mock/1");
-const m2 = require("./mock/2");
-const m3 = require("./mock/3");
-const m4 = require("./mock/4");
+const mqtt = require('mqtt')
+const m1 = require('./mock/1')
+const m2 = require('./mock/2')
+const m3 = require('./mock/3')
+const m4 = require('./mock/4')
 
-const client = mqtt.connect("mqtt://127.0.0.1:8002", {
-  username: "user",
-  password: "123456",
-});
-client.on("connect", function () {
-  console.log("服务器连接成功");
-  console.log(client.options.clientId);
-  test(client);
-});
+const client = mqtt.connect('mqtt://127.0.0.1:8002', {
+  username: 'user',
+  password: '123456'
+})
+client.on('connect', function () {
+  console.log('服务器连接成功')
+  console.log(client.options.clientId)
+  test(client)
+})
 
 function test(client) {
   setInterval(() => {
-    [...m1, ...m2, ...m3, ...m4].forEach((m) => {
-      client.publish("active", JSON.stringify(m), () => {});
-    });
-  }, 1000);
+    ;[...m1, ...m2, ...m3, ...m4].forEach((m) => {
+      client.publish('active', JSON.stringify(m), () => {})
+      // client.publish('active', JSON.stringify(m), () => {})
+    })
+  }, 1000)
 }
--- subscribe.js
+++ subscribe.js
@@ -1,17 +1,17 @@
-const mqtt = require("mqtt");
-const client = mqtt.connect('mqtt://127.0.0.1:8002',{
-  username: "user",
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://127.0.0.1:8002', {
+  username: 'user',
   password: '123456'
-});
-client.on("connect", function() {
-  console.log("服务器连接成功");
-  console.log(client.options.clientId);
-  client.subscribe("active", { qos: 1 }); // 订阅text消息
-});
-client.on("message", function(top, message) {
-  console.log("当前topic：", top);
-  console.log("内容：", message.toString());
-});
+})
+client.on('connect', function () {
+  console.log('服务器连接成功')
+  console.log(client.options.clientId)
+  client.subscribe('active', { qos: 1, }) // 订阅text消息
+})
+client.on('message', function (top, message) {
+  console.log('当前topic：', top)
+  console.log('内容：', message.toString())
+})
 // const mqtt = require('./node_modules/mqtt/dist/mqtt.js')
 // const client = mqtt.connect("mqtt://123.124.196.193:2102"); //指定服务端地址和端口
 // const client = mqtt.connect("ws://127.0.0.1:8002/mqtt"); //连接到mqtt服务端
